a5d1d861b37ed739f91f84c57c883a34614b419a
SorenSawback <97757105+SorenSawback@users.noreply.github.com>
2023-10-18 15:36:35 -0600

    Creating webgl varient

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
-
 use paga::common::*;
 use paga::screens::game_screen::*;
+use std::{borrow::BorrowMut, rc::Rc};
 
 use error_iter::ErrorIter as _;
 use game_loop::{game_loop, Time, TimeTrait as _};
@@ -28,7 +28,23 @@ fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
     }
 }
 
-fn main() -> Result<(), Error> {
+fn main() {
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
+
+        wasm_bindgen_futures::spawn_local(run());
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+
+        pollster::block_on(run());
+    }
+}
+async fn run() {
     env_logger::init();
     let event_loop = EventLoop::new();
 
@@ -48,14 +64,61 @@ fn main() -> Result<(), Error> {
             .build(&event_loop)
             .unwrap()
     };
+    let window = Rc::new(window);
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowExtWebSys;
 
-    let pixels = {
+        // Retrieve current width and height dimensions of browser client window
+        let get_window_size = || {
+            let client_window = web_sys::window().unwrap();
+            LogicalSize::new(
+                client_window.inner_width().unwrap().as_f64().unwrap(),
+                client_window.inner_height().unwrap().as_f64().unwrap(),
+            )
+        };
+
+        let window = Rc::clone(&window);
+
+        // Initialize winit window with current dimensions of browser client
+        window.set_inner_size(get_window_size());
+
+        let client_window = web_sys::window().unwrap();
+
+        // Attach winit canvas to body element
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+
+        // Listen for resize event on browser client. Adjust winit window dimensions
+        // on event trigger
+        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+            let size = get_window_size();
+            window.set_inner_size(size)
+        }) as Box<dyn FnMut(_)>);
+        client_window
+            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+            .unwrap();
+        closure.forget();
+    }
+
+    let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
+        Pixels::new_async(WIDTH as u32, HEIGHT as u32, surface_texture)
+            .await
+            .expect("Pixels error")
     };
 
     let game = GameScreen::new(pixels, debug);
+    let window = window.clone();
 
     game_loop(
         event_loop,
